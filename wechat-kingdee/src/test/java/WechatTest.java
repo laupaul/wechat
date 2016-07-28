612d8002f5b50597f4e86cee9f21999ae66d01a7
laupaul <l_m_peng@163.com>
2016-07-28 18:06:38 +0800

    11

---
+++ wechat-kingdee/src/test/java/WechatTest.java
@@ -0,0 +1,11 @@
+import me.hao0.wechat.core.Wechat;
+import me.hao0.wechat.core.WechatBuilder;
+
+
+public class WechatTest {
+
+	public static void main(String[] args) {
+		Wechat wechat = WechatBuilder.newBuilder("", "").build();
+		
+	}
+}
